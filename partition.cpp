a750a83f7d6d7534f52c58321ea35a8ba57f7336
CosmicDan <cosmicdan@cosmicdan-vm.(none)>
2013-01-10 14:32:49 +1100

    Restore standard backup/restore functions (compression, splitting support)

--- partition.cpp
+++ partition.cpp
@@ -1148,8 +1148,8 @@ bool TWPartition::Wipe_Data_Without_Wiping_Media() {
 
 bool TWPartition::Backup_Tar(string backup_folder) {
 	char back_name[255], split_index[5];
-	string Full_FileName, Split_FileName, Command;
-	int index, backup_count;
+	string Full_FileName, Split_FileName, Tar_Args, Command;
+	int use_compression, index, backup_count;
 	struct stat st;
 	unsigned long long total_bsize = 0, file_size;
 
@@ -1164,16 +1164,53 @@ bool TWPartition::Backup_Tar(string backup_folder) {
 		ui_print("Backing up %s...\n", Display_Name.c_str());
 	}
 
+	DataManager::GetValue(TW_USE_COMPRESSION_VAR, use_compression);
+	if (use_compression)
+		Tar_Args = "-cz";
+	else
+		Tar_Args = "-c";
+
 	sprintf(back_name, "%s.%s.win", Backup_Name.c_str(), Current_File_System.c_str());
 	Backup_FileName = back_name;
 
-    Full_FileName = backup_folder + "/" + Backup_FileName;
-    Command = "cd " + Backup_Path + " && tar -cf - ./* | gzip > " + Full_FileName;
-	LOGI("Backup command: '%s'\n", Command.c_str());
-	system(Command.c_str());
-	if (TWFunc::Get_File_Size(Full_FileName) == 0) {
-		LOGE("Backup file size is 0 bytes (possible tar/gzip issue).\n", Full_FileName.c_str());
-		return false;
+	if (Backup_Size > MAX_ARCHIVE_SIZE) {
+		// This backup needs to be split into multiple archives
+		ui_print("Breaking backup file into multiple archives...\nGenerating file lists\n");
+		sprintf(back_name, "%s", Backup_Path.c_str());
+		backup_count = MakeList::Make_File_List(back_name);
+		if (backup_count < 1) {
+			LOGE("Error generating file list!\n");
+			return false;
+		}
+		for (index=0; index<backup_count; index++) {
+			sprintf(split_index, "%03i", index);
+			Full_FileName = backup_folder + "/" + Backup_FileName + split_index;
+			Command = "tar " + Tar_Args + " -f '" + Full_FileName + "' -T /tmp/list/filelist" + split_index;
+			LOGI("Backup command: '%s'\n", Command.c_str());
+			ui_print("Backup archive %i of %i...\n", (index + 1), backup_count);
+			system(Command.c_str()); // sending backup command formed earlier above
+
+			file_size = TWFunc::Get_File_Size(Full_FileName);
+			if (file_size == 0) {
+				LOGE("Backup file size for '%s' is 0 bytes.\n", Full_FileName.c_str()); // oh noes! file size is 0, abort! abort!
+				return false;
+			}
+			total_bsize += file_size;
+		}
+		ui_print(" * Total size: %llu bytes.\n", total_bsize);
+		system("cd /tmp && rm -rf list");
+	} else {
+		Full_FileName = backup_folder + "/" + Backup_FileName;
+		if (Has_Data_Media)
+			Command = "cd " + Backup_Path + " && tar " + Tar_Args + " ./ --exclude='media*' -f '" + Full_FileName + "'";
+		else
+			Command = "cd " + Backup_Path + " && tar " + Tar_Args + " -f '" + Full_FileName + "' ./*";
+		LOGI("Backup command: '%s'\n", Command.c_str());
+		system(Command.c_str());
+		if (TWFunc::Get_File_Size(Full_FileName) == 0) {
+			LOGE("Backup file size for '%s' is 0 bytes.\n", Full_FileName.c_str());
+			return false;
+		}
 	}
 	return true;
 }
@@ -1247,9 +1284,29 @@ bool TWPartition::Restore_Tar(string restore_folder, string Restore_File_System)
 	TWFunc::GUI_Operation_Text(TW_RESTORE_TEXT, Display_Name, "Restoring");
 	ui_print("Restoring %s...\n", Display_Name.c_str());
 	Full_FileName = restore_folder + "/" + Backup_FileName;
-	Command = "cd " + Backup_Path + " && gunzip -c '" + Full_FileName + "' | tar -xf -";
-	LOGI("Restore command: '%s'\n", Command.c_str());
-    system(Command.c_str());
+	if (!TWFunc::Path_Exists(Full_FileName)) {
+		// Backup is multiple archives
+		LOGI("Backup is multiple archives.\n");
+		sprintf(split_index, "%03i", index);
+		Full_FileName = restore_folder + "/" + Backup_FileName + split_index;
+		while (TWFunc::Path_Exists(Full_FileName)) {
+			ui_print("Restoring archive %i...\n", index + 1);
+			Command = "tar -xf '" + Full_FileName + "'";
+			LOGI("Restore command: '%s'\n", Command.c_str());
+			system(Command.c_str());
+			index++;
+			sprintf(split_index, "%03i", index);
+			Full_FileName = restore_folder + "/" + Backup_FileName + split_index;
+		}
+		if (index == 0) {
+			LOGE("Error locating restore file: '%s'\n", Full_FileName.c_str());
+			return false;
+		}
+	} else {
+		Command = "cd " + Backup_Path + " && tar -xf '" + Full_FileName + "'";
+		LOGI("Restore command: '%s'\n", Command.c_str());
+		system(Command.c_str());
+	}
 	return true;
 }
 
